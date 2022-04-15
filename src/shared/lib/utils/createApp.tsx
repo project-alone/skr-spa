4bfc7458932f17efbaded8068092d0561e417079
yikl1004 <yikl1004@gmail.com>
2022-04-15 13:47:49 +0900

    createApp 함수 추가

--- src/shared/lib/utils/createApp.tsx
+++ src/shared/lib/utils/createApp.tsx
@@ -1,19 +1,30 @@
 /* eslint-disable react/jsx-key */
 
+// core
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// providers
 import { Provider as ReduxProvider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import { BrowserRouter } from 'react-router-dom'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import ThemeProvider from '@theme/ThemeProvider'
 import { ModalProvider } from '@lib/modal'
-import { SidebarProvider } from '@context/Sidebar'
-import { store, createPersistor } from '@store/index'
-import { Loading } from '@components/common'
 import { ComposeProvider } from '@lib/utils'
+import { SidebarProvider } from '@context/Sidebar'
+/** @see https://iamhosseindhv.com/notistack/api */
+import { SnackbarProvider } from 'notistack'
+
+// date util
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+
+// store
+import { store, createPersistor } from '@store/index'
+
+// loading indecator
+import { Loading } from '@components/common'
 
 /**
  * StrictMode: 'react'의 잠재적 문재점 확인
@@ -40,9 +51,20 @@ const providers = [
 	<BrowserRouter />,
 ]
 
+const container =
+	document.getElementById('root') ??
+	(() => {
+		const container = document.createElement('div')
+		document.body.appendChild(container)
+
+		return container
+	})()
+
 export const createApp = (element: React.ReactElement) => {
 	ReactDOM.render(
-		<ComposeProvider providers={providers}>{element}</ComposeProvider>,
-		document.getElementById('root'),
+		<ComposeProvider providers={providers}>
+			<SnackbarProvider maxSnack={10}>{element}</SnackbarProvider>
+		</ComposeProvider>,
+		container,
 	)
 }
