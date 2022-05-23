738fa772c3c4eaaf41377e940d8d0652e29f12b8
yikl1004 <yikl1004@gmail.com>
2022-05-23 09:47:33 +0900

    dependency 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 // styles
 import { createApp } from '@lib/utils/createApp'
 import App from '@root/App'
-import '@lib/utils/chart'
 import 'nprogress/nprogress.css'
 
 // using env variables, because use ESModule
